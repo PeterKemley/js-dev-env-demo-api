dfa7753cb03f2963a5735015e7de879dfee3b969
PeterKemley <peter.kemley@accenture.com>
2020-01-09 14:10:26 +0000

    Changes

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('API')
 });
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Craggle","McNiggle":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Joph","Hell":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Jamus","Pinkus":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
